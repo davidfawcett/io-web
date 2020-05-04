c98fa8e1eca73ef7119356567d29b1f2afae5349
David Fawcett <omniwoof@gmail.com>
2020-05-04 22:53:03 +1000

    working on adding routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,29 +2,39 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatInputModule } from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatRadioModule} from '@angular/material/radio';
-import {MatCardModule} from '@angular/material/card';
-import {MatTooltipModule} from '@angular/material/tooltip';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatCardModule } from '@angular/material/card';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { Router, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { NoPageFoundComponent } from './no-page-found/no-page-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    NoPageFoundComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatButtonModule, 
+    MatButtonModule,
     MatCheckboxModule,
     MatInputModule,
     MatFormFieldModule,
     MatRadioModule,
     MatCardModule,
-    MatTooltipModule
+    MatTooltipModule,
+    RouterModule.forRoot([
+      { path: '', component: AppComponent },
+      { path: 'about', component: AboutComponent },
+      { path: '**', component: NoPageFoundComponent },
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
